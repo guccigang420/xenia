d6d913e72e03d708d12be2cd6c7c9802e987e705
guccigang420 <>
2023-09-26 00:13:33 +0200

    Added path/file names to GetInfo in filesystem_posix.cc

--- src/xenia/base/filesystem_posix.cc
+++ src/xenia/base/filesystem_posix.cc
@@ -202,6 +202,8 @@ bool GetInfo(const std::filesystem::path& path, FileInfo* out_info) {
       out_info->type = FileInfo::Type::kFile;
       out_info->total_size = st.st_size;
     }
+    out_info->path = path.parent_path();
+    out_info->name = path.filename();
     out_info->create_timestamp = convertUnixtimeToWinFiletime(st.st_ctime);
     out_info->access_timestamp = convertUnixtimeToWinFiletime(st.st_atime);
     out_info->write_timestamp = convertUnixtimeToWinFiletime(st.st_mtime);
