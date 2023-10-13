50f0d22c007124ac34eddc6bc09e638d884067f2
guccigang420 <>
2023-10-13 20:55:36 +0200

    Test commit avoiding the usage of deleted object current_thread_

--- src/xenia/base/threading_posix.cc
+++ src/xenia/base/threading_posix.cc
@@ -1146,14 +1146,7 @@ Thread* Thread::GetCurrentThread() {
 }
 
 void Thread::Exit(int exit_code) {
-  if (current_thread_) {
-    current_thread_->Terminate(exit_code);
-  } else {
-    // Should only happen with the main thread
-    pthread_exit(reinterpret_cast<void*>(exit_code));
-  }
-  // Function must not return
-  assert_always();
+  pthread_exit(reinterpret_cast<void*>(exit_code));
 }
 
 void set_name(const std::string_view name) {
